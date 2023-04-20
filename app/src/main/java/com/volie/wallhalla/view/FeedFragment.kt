afa896565e91688ad7a15ee1406d07d838936feb
harun <harunuyan6@gmail.com>
2023-04-20 15:00:00 +0300

    Added Feed & Favorite Fragment

---
+++ app/src/main/java/com/volie/wallhalla/view/FeedFragment.kt
@@ -0,0 +1,29 @@
+package com.volie.wallhalla.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.volie.wallhalla.databinding.FragmentFeedBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FeedFragment : Fragment() {
+    private var _mBinding: FragmentFeedBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentFeedBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
+}
