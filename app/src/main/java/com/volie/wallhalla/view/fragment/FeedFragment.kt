a317f9a177630e60e3019f6637eaa7e3d67de125
harun <harunuyan6@gmail.com>
2023-04-20 17:56:41 +0300

    Added Feed Fragment & Search Fragment for tab layout

--- app/src/main/java/com/volie/wallhalla/view/fragment/FeedFragment.kt
+++ app/src/main/java/com/volie/wallhalla/view/fragment/FeedFragment.kt
@@ -1,14 +1,16 @@
-package com.volie.wallhalla.view
+package com.volie.wallhalla.view.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.volie.wallhalla.databinding.FragmentFavoriteBinding
+import com.volie.wallhalla.databinding.FragmentFeedBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class FavoriteFragment : Fragment() {
-    private var _mBinding: FragmentFavoriteBinding? = null
+@AndroidEntryPoint
+class FeedFragment : Fragment() {
+    private var _mBinding: FragmentFeedBinding? = null
     private val mBinding get() = _mBinding!!
 
     override fun onCreateView(
@@ -16,7 +18,7 @@ class FavoriteFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _mBinding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        _mBinding = FragmentFeedBinding.inflate(inflater, container, false)
         return mBinding.root
     }
 
--- app/src/main/java/com/volie/wallhalla/view/fragment/SearchFragment.kt
+++ app/src/main/java/com/volie/wallhalla/view/fragment/SearchFragment.kt
@@ -1,16 +1,14 @@
-package com.volie.wallhalla.view
+package com.volie.wallhalla.view.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.volie.wallhalla.databinding.FragmentFeedBinding
-import dagger.hilt.android.AndroidEntryPoint
+import com.volie.wallhalla.databinding.FragmentSearchBinding
 
-@AndroidEntryPoint
-class FeedFragment : Fragment() {
-    private var _mBinding: FragmentFeedBinding? = null
+class SearchFragment : Fragment() {
+    private var _mBinding: FragmentSearchBinding? = null
     private val mBinding get() = _mBinding!!
 
     override fun onCreateView(
@@ -18,12 +16,12 @@ class FeedFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _mBinding = FragmentFeedBinding.inflate(inflater, container, false)
+        _mBinding = FragmentSearchBinding.inflate(inflater, container, false)
         return mBinding.root
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _mBinding = null
     }
 }
