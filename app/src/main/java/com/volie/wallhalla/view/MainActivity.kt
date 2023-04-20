0aa94321be10b39b23821bc950520455c6023623
harun <harunuyan6@gmail.com>
2023-04-20 15:01:01 +0300

    Added Bottom Navigation View

--- app/src/main/java/com/volie/wallhalla/view/MainActivity.kt
+++ app/src/main/java/com/volie/wallhalla/view/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.volie.wallhalla.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.volie.wallhalla.R
 import com.volie.wallhalla.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,10 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+        setupBottomNavView()
+    }
+
+    private fun setupBottomNavView() {
+        mBinding.bottomNavView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.feedFragment -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.feedFragment)
+                    true
+                }
+
+                R.id.favoriteFragment -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.favoriteFragment)
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _mBinding = null
     }
+
 }
