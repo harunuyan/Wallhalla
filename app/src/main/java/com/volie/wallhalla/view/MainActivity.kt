a09a9ac7f96c7be360df9f8e9b2903fade5ff8bf
harun <harunuyan6@gmail.com>
2023-04-20 14:07:42 +0300

    Added Dagger Hilt application class and updated manifest file

--- app/src/main/java/com/volie/wallhalla/view/MainActivity.kt
+++ app/src/main/java/com/volie/wallhalla/view/MainActivity.kt
@@ -3,7 +3,9 @@ package com.volie.wallhalla.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.volie.wallhalla.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private var _mBinding: ActivityMainBinding? = null
     private val mBinding get() = _mBinding!!
