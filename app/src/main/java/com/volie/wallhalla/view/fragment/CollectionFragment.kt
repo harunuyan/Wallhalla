d8fcd6336126e9fcd07e37a0d7bf201e21e5ebc3
harun <harunuyan6@gmail.com>
2023-04-21 15:41:17 +0300

    changed feed to home fragment & added collection, setting fragments in bottom navigation

---
+++ app/src/main/java/com/volie/wallhalla/view/fragment/CollectionFragment.kt
@@ -0,0 +1,7 @@
+package com.volie.wallhalla.view.fragment
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CollectionFragment : Fragment()
---
+++ app/src/main/java/com/volie/wallhalla/view/fragment/SettingFragment.kt
@@ -0,0 +1,8 @@
+package com.volie.wallhalla.view.fragment
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingFragment : Fragment() {
+}
