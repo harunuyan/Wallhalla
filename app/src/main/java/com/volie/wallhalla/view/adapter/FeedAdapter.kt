d716aa43966f705f55066cd952e38fc8b653f972
harun <harunuyan6@gmail.com>
2023-04-21 23:29:09 +0300

    Added Feed Adapter for Recycler View

---
+++ app/src/main/java/com/volie/wallhalla/view/adapter/FeedAdapter.kt
@@ -0,0 +1,50 @@
+package com.volie.wallhalla.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.volie.wallhalla.data.model.Photo
+import com.volie.wallhalla.databinding.ItemFeedBinding
+
+class FeedAdapter : ListAdapter<Photo, FeedAdapter.FeedViewHolder>(
+    PhotoDiffCallBack()
+) {
+    inner class FeedViewHolder(private val binding: ItemFeedBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            val item = currentList[position]
+            with(binding) {
+                Glide.with(root.context)
+                    .load(item.url)
+                    .into(ivFeedItem)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
+        val binding = ItemFeedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FeedViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int {
+        return currentList.size
+    }
+}
+
+class PhotoDiffCallBack : DiffUtil.ItemCallback<Photo>() {
+    override fun areItemsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+        return oldItem == newItem
+    }
+
+}
--- app/src/main/java/com/volie/wallhalla/view/fragment/FeedFragment.kt
+++ app/src/main/java/com/volie/wallhalla/view/fragment/FeedFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.volie.wallhalla.databinding.FragmentFeedBinding
+import com.volie.wallhalla.view.adapter.FeedAdapter
+import com.volie.wallhalla.view.viewmodel.FeedViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FeedFragment : Fragment() {
     private var _mBinding: FragmentFeedBinding? = null
     private val mBinding get() = _mBinding!!
+    private val mViewModel: FeedViewModel by viewModels()
+    private val mAdapter: FeedAdapter by lazy {
+        FeedAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
