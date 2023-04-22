0efb15d958bc2c5695f9f82f73cf3ad26799ca9d
harun <harunuyan6@gmail.com>
2023-04-22 17:17:40 +0300

    Added feature to download wallpapers to phone gallery

--- app/src/main/java/com/volie/wallhalla/view/fragment/PhotoDetailsFragment.kt
+++ app/src/main/java/com/volie/wallhalla/view/fragment/PhotoDetailsFragment.kt
@@ -1,13 +1,35 @@
 package com.volie.wallhalla.view.fragment
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.ConnectivityManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.volie.wallhalla.R
 import com.volie.wallhalla.databinding.FragmentPhotoDetailsBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+import java.io.InputStream
 
 class PhotoDetailsFragment : Fragment() {
     private var _mBinding: FragmentPhotoDetailsBinding? = null
@@ -26,11 +48,124 @@ class PhotoDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        with(mBinding) {
+            ivBackDetails.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+
         arguments?.let {
             args = PhotoDetailsFragmentArgs.fromBundle(it)
         }
 
+        mBinding.ivDownloadDetails.setOnClickListener {
+            downloadImage()
+        }
+
         getDetails()
+    }
+
+    private fun downloadImage() {
+        if (!isNetworkAvailable()) {
+            mBinding.ivDownloadDetails.setImageResource(R.drawable.ic_download_failed)
+            Toast.makeText(requireContext(), "No internet connection", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val client = OkHttpClient()
+
+            val request = Request.Builder()
+                .url(args.info.src?.large2x.toString())
+                .build()
+
+            val response = client.newCall(request).execute()
+
+            val inputStream: InputStream? = response.body?.byteStream()
+            val image = BitmapFactory.decodeStream(inputStream)
+
+            val displayName = "Title"
+            val mimeType = "image/jpeg"
+            val contentResolver = context?.contentResolver
+            val contentValues = ContentValues().apply {
+                put(MediaStore.Images.Media.DISPLAY_NAME, displayName)
+                put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+                put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
+            }
+
+            val uri = contentResolver?.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+
+            var hasError = false
+            uri?.let {
+                contentResolver.openOutputStream(it, "w").use { outputStream ->
+                    try {
+                        if (image.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)) {
+                            withContext(Dispatchers.Main) {
+                                mBinding.ivDownloadDetails.setImageResource(R.drawable.ic_download_succesfully)
+                                showDownloadNotification()
+                            }
+                        } else {
+                            hasError = true
+                        }
+                    } catch (e: IOException) {
+                        hasError = true
+                    }
+                }
+            } ?: run {
+                hasError = true
+            }
+
+            if (hasError) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(requireContext(), "Download failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            inputStream?.close()
+        }
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            requireContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
+
+    private fun showDownloadNotification() {
+        val notificationManager = ContextCompat.getSystemService(
+            requireContext(), NotificationManager::class.java
+        ) as NotificationManager
+
+        val notificationId = 1
+        val channelId = "channelId"
+        val title = "Image downloaded successfully"
+
+// Create a notification channel for Android 8.0 and higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel name",
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "Channel description"
+            }
+
+            notificationManager.createNotificationChannel(channel)
+        }
+
+// Create a notification builder
+        val builder = NotificationCompat.Builder(requireContext(), channelId)
+            .setSmallIcon(R.drawable.ic_download)
+            .setContentTitle(title)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+
+// Show the notification
+        notificationManager.notify(notificationId, builder.build())
     }
 
     private fun getDetails() {
