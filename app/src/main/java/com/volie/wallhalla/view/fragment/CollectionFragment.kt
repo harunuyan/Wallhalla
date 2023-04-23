f85d997411b31108f8b43af91947d7cdf92e2e73
harun <harunuyan6@gmail.com>
2023-04-24 02:51:28 +0300

    Added Collection Item Layout for Collection Fragment

--- app/src/main/java/com/volie/wallhalla/view/fragment/CollectionFragment.kt
+++ app/src/main/java/com/volie/wallhalla/view/fragment/CollectionFragment.kt
@@ -1,7 +1,29 @@
 package com.volie.wallhalla.view.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.volie.wallhalla.databinding.FragmentCollectionBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CollectionFragment : Fragment()
+class CollectionFragment : Fragment() {
+    private var _mBinding: FragmentCollectionBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentCollectionBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
+}
