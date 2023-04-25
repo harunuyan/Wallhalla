9e09d415553447ebcd73cb88f533f5580b8bc20f
harun <harunuyan6@gmail.com>
2023-04-26 02:22:13 +0300

    Added Collection Featured API Service

--- app/src/main/java/com/volie/wallhalla/data/service/WallpaperService.kt
+++ app/src/main/java/com/volie/wallhalla/data/service/WallpaperService.kt
@@ -1,6 +1,7 @@
 package com.volie.wallhalla.data.service
 
 import com.volie.wallhalla.data.model.CuratedResponse
+import com.volie.wallhalla.data.model.collection.CollectionResponse
 import com.volie.wallhalla.util.Constant.AUTHORIZATION
 import retrofit2.Response
 import retrofit2.http.GET
@@ -18,4 +19,14 @@ interface WallpaperService {
         @Header("Authorization")
         authorization: String = AUTHORIZATION
     ): Response<CuratedResponse>
+
+    @GET("collections/featured")
+    suspend fun getFeaturedCollections(
+        @Query("page")
+        page: Int? = 1,
+        @Query("per_page")
+        perPage: Int? = 40,
+        @Header("Authorization")
+        authorization: String = AUTHORIZATION
+    ): Response<CollectionResponse>
 }
