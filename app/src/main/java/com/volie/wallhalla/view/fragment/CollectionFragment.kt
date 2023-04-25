5c31f467650303fea807ee47c4885b9c3c35daa8
harun <harunuyan6@gmail.com>
2023-04-26 01:57:35 +0300

    Added CollectionFeedFragment & CollectionFragment

--- app/src/main/java/com/volie/wallhalla/view/fragment/CollectionFragment.kt
+++ app/src/main/java/com/volie/wallhalla/view/fragment/CollectionFragment.kt
@@ -5,13 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.volie.wallhalla.databinding.FragmentCollectionBinding
+import com.volie.wallhalla.view.adapter.CollectionAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CollectionFragment : Fragment() {
     private var _mBinding: FragmentCollectionBinding? = null
     private val mBinding get() = _mBinding!!
+    private val mAdapter by lazy {
+        CollectionAdapter(
+            onItemClick = {
+                val action =
+                    CollectionFragmentDirections.actionCollectionFragmentToCollectionFeedFragment()
+                findNavController().navigate(action)
+            }
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +32,13 @@ class CollectionFragment : Fragment() {
     ): View {
         _mBinding = FragmentCollectionBinding.inflate(inflater, container, false)
         return mBinding.root
+    }
+
+    private fun setupRecyclerView() {
+        with(mBinding) {
+            rvCollection.adapter = mAdapter
+            rvCollection.layoutManager = LinearLayoutManager(requireContext())
+        }
     }
 
     override fun onDestroy() {
