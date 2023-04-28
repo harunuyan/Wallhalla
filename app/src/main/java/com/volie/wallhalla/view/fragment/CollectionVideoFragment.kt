d6cbfb0d93ac8dda142477c2b35c8336f9321e03
harun <harunuyan6@gmail.com>
2023-04-28 16:53:32 +0300

    Added Fragment Collection Video layout

--- app/src/main/java/com/volie/wallhalla/view/fragment/CollectionVideoFragment.kt
+++ app/src/main/java/com/volie/wallhalla/view/fragment/CollectionVideoFragment.kt
@@ -1,5 +1,27 @@
 package com.volie.wallhalla.view.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.volie.wallhalla.databinding.FragmentCollectionVideoBinding
 
-class CollectionVideoFragment : Fragment()
+class CollectionVideoFragment : Fragment() {
+    private var _mBinding: FragmentCollectionVideoBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentCollectionVideoBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
+}
