5ef2c3fd9613d7e83c7002ba9bab88e15f8bf609
harun <harunuyan6@gmail.com>
2023-05-03 23:29:26 +0300

    Added Setting Fragment & designed layout

--- app/src/main/java/com/volie/wallhalla/view/fragment/SettingFragment.kt
+++ app/src/main/java/com/volie/wallhalla/view/fragment/SettingFragment.kt
@@ -1,8 +1,50 @@
 package com.volie.wallhalla.view.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.volie.wallhalla.R
+import com.volie.wallhalla.databinding.BottomSheetLayoutChooseThemeBinding
+import com.volie.wallhalla.databinding.FragmentSettingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SettingFragment : Fragment() {
+    private var _mBinding: FragmentSettingBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentSettingBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(mBinding) {
+            llTheme.setOnClickListener {
+                val bottomSheetDialog =
+                    BottomSheetDialog(requireContext(), R.style.BottomSheetDialogTheme)
+                val bottomSheetView = LayoutInflater.from(requireContext())
+                    .inflate(R.layout.bottom_sheet_layout_choose_theme, root, false)
+                bottomSheetDialog.setContentView(bottomSheetView)
+                bottomSheetDialog.show()
+
+                val mBindingBottomSheet = BottomSheetLayoutChooseThemeBinding.bind(bottomSheetView)
+
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
 }
