5e216ec2da3a3b1364ead789e1c2f294038d6b3f
harun <harunuyan6@gmail.com>
2023-07-31 11:00:22 +0300

    Hidden API key

--- app/src/main/java/com/volie/wallhalla/util/Constant.kt
+++ app/src/main/java/com/volie/wallhalla/util/Constant.kt
@@ -1,12 +1,15 @@
 package com.volie.wallhalla.util
 
+import com.volie.wallhalla.BuildConfig.WALLHALLA_AUTHORIZATION
 object Constant {
 
     const val BASE_URL = "https://api.pexels.com/v1/"
-    const val AUTHORIZATION = "nnci2UOX6KjeavgnJvwGAsX4G3wVQ9GFdn3UHTI0PlTVpsND5kLGdicw"
+    const val AUTHORIZATION = WALLHALLA_AUTHORIZATION
     const val DATABASE_NAME = "curated_response_database"
     const val PREF_NAME = "myPref"
     const val GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=com.volie.wallhalla"
     const val GITHUB_GIST_URL = "https://gist.github.com/harunuyan/d55c56eafcf97d4e4234d5590e1ee2aa"
     const val GITHUB_REPO_URL = "https://github.com/harunuyan/Wallhalla"
+
+    const val DOUBLE_CLICK_INTERVAL = 1500
 }
